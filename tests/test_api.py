434d1ade565190904a5a23b8f0970e5720d3b47a
rossatuai <rossatuai@gmail.com>
2026-05-16 22:03:58 +0100

    modified dockerhub, mlops-pipeline to run after unit tests in github actions

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,3 +20,20 @@ def test_invalid_route():
     response = client.get("/fake")
 
     assert response.status_code == 404
+
+
+
+
+def test_prediction_route():
+    client = app.test_client()
+
+    response = client.post(
+        "/predict",
+        json={"text": "I love this movie"}
+    )
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert "prediction" in data
